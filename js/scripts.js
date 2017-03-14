c89ec4ff52621720f87c11dc7a37822209924cc1
philippzalewski <kontakt@centrum-ngo.pl>
2017-03-14 15:24:13 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@
 
 var name = prompt('Wpisz swoje imię');
 // Wyłączam funkcję allert Kodilla alert('Witaj, ' + name);
+// Komentarz Nr 1
 console.log('Witaj, ' + name);
+
+/*
+Komentarz Nr 2
+*/
+  
